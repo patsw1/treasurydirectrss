f8f66407ccb3c5e3e017ed2131684b9ba616b5f2
Patrick Sweeney <psweeney@nyc.rr.com>
2022-01-03 10:27:25 -0500

    first commit

---
+++ src/main/java/com/example/treasurydirectrss/TreasurydirectrssApplication.java
@@ -0,0 +1,13 @@
+package com.example.treasurydirectrss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreasurydirectrssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreasurydirectrssApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/treasurydirectrss/TreasurydirectrssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.treasurydirectrss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreasurydirectrssApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
